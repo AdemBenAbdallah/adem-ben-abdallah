56ebef5920a04d775ecb4a7432eb6e334ba0dea6
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-08-06 16:00:42 +0100

    add callout

--- src/components/global/mdx-components.tsx
+++ src/components/global/mdx-components.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import * as runtime from "react/jsx-runtime";
+import { Callout } from "../ui/callout";
 
 const useMDXComponent = (code: string) => {
   const fn = new Function(code);
@@ -8,6 +9,7 @@ const useMDXComponent = (code: string) => {
 
 const components = {
   Image,
+  Callout,
 };
 
 interface MdxProps {
