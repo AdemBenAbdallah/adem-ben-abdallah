674d4ebda23b867adef0ebd6df1b771f1db9ba27
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-08-07 09:44:34 +0100

    remove warn in next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
 import { build } from "velite";
 
 /** @type {import('next').NextConfig} */
-export default {
-  // othor next config here...
+
+const webpackConfig = {
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
     return config;
   },
 };
+export default webpackConfig;
 
 class VeliteWebpackPlugin {
   static started = false;
   apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
     compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
       if (VeliteWebpackPlugin.started) return;
       VeliteWebpackPlugin.started = true;
