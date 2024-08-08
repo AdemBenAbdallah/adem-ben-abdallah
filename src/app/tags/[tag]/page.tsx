cbddf7913d91423b98ec948b2cf6b223e420220b
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-08-08 16:22:07 +0100

    add the tags fucntionnaliyt

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,32 +1,38 @@
 import { posts } from "#site/content";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PostItem, Tag } from "@/components/ui/post-item";
-import { getAllTags, sortPosts, sortTagsByCount } from "@/lib/utils";
+import { getAllTags, getPostsByTagSlug, sortTagsByCount } from "@/lib/utils";
+import { slug } from "github-slugger";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "My blog",
-  description: "This is a description",
-};
-
-const POSTS_PER_PAGE = 15;
-
-interface BlogPageProps {
-  searchParams: {
-    page?: string;
+interface TagPageProps {
+  params: {
+    tag: string;
   };
 }
 
-export default async function BlogPage({ searchParams }: BlogPageProps) {
-  const currentPage = Number(searchParams?.page) || 1;
-  const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  // const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+export async function generateMetadata({
+  params,
+}: TagPageProps): Promise<Metadata> {
+  const { tag } = params;
+  return {
+    title: tag,
+    description: `Posts on the topic of ${tag}`,
+  };
+}
 
-  const displayPosts = sortedPosts.slice(
-    POSTS_PER_PAGE * (currentPage - 1),
-    POSTS_PER_PAGE * currentPage
-  );
+export const generateStaticParams = () => {
+  const tags = getAllTags(posts);
+  const paths = Object.keys(tags).map((tag) => ({ tag: slug(tag) }));
+  return paths;
+};
 
+export default function TagPage({ params }: TagPageProps) {
+  const { tag } = params;
+  const title = tag.split("-").join(" ");
+
+  const allPosts = getPostsByTagSlug(posts, tag);
+  const displayPosts = allPosts.filter((post) => post.published);
   const tags = getAllTags(posts);
   const sortedTags = sortTagsByCount(tags);
 
@@ -34,16 +40,15 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
-          <p className="text-xl text-muted-foreground">
-            My ramblings on all things web dev.
-          </p>
+          <h1 className="inline-block font-black text-4xl lg:text-5xl capitalize">
+            {title}
+          </h1>
         </div>
       </div>
-      <div className="grid grid-cols-12 gap-3 mt-8 ">
+      <div className="grid grid-cols-12 gap-3 mt-8">
         <div className="col-span-12 col-start-1 sm:col-span-8">
           {displayPosts?.length > 0 ? (
-            <ul className="flex flex-col gap-5">
+            <ul className="flex flex-col">
               {displayPosts.map((post) => {
                 const { slug, date, title, description, tags } = post;
                 return (
@@ -68,8 +73,8 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
             <CardTitle>Tags</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-wrap gap-2">
-            {sortedTags?.map((tag) => (
-              <Tag tag={tag} key={tag} count={tags[tag]} />
+            {sortedTags?.map((t) => (
+              <Tag tag={t} key={t} count={tags[t]} current={slug(t) === tag} />
             ))}
           </CardContent>
         </Card>
